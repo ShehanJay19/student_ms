6687c71eb827449c9a563fb4685ca07ddeb444a3
Shehan Jayasinghe <shehanjaya1921@gmail.com>
2025-04-05 17:43:46 +0530

    some css added to navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,44 +11,40 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        </head>
-    <body>
-     <!-- Navigation Bar -->
-     <nav class="navbar navbar-expand-lg bg-transparent">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Sipvimana</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-           
+        <!--custom css -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+      </head>
+   
+   
+      <body>
 
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="">Students</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Teachers</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Courses</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-              </li>
-              
-            </ul>
+        <nav class="navbar navbar-expand-lg">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="#">Sipvimana</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav ms-auto">
+                      <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Students</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Teachers</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Courses</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Contact</a>
+                      </li>
+                  </ul>
+              </div>
           </div>
-        </div>
       </nav>
-
-     <!-- End Navigation Bar -->
 
      <!-- Welcome Area -->
      <section id="welcome">
@@ -110,7 +106,7 @@
         <!-- End Detail area -->
 
         <!--Contatct Area -->
-        
+
         <section id="contact" class="contact-area">
 
             <p></p>
